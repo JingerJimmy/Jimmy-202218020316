d90860bae38e793517cd3c00f7a3d9a125190f4e
Jinger <1399756302@qq.com>
2024-05-15 14:45:56 +0800

    test

---
+++ test/VernacularJazzTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VernacularJazzTest {
+
+    @org.junit.jupiter.api.Test
+    void blackBottom() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void appleJack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void cakeWalk() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void charleston() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void drunkenSailor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hitchHiker() {
+    }
+
+    @Test
+    void testBlackBottom() {
+    }
+
+    @Test
+    void testAppleJack() {
+    }
+
+    @Test
+    void testCakeWalk() {
+    }
+
+    @Test
+    void testCharleston() {
+    }
+
+    @Test
+    void testDrunkenSailor() {
+    }
+
+    @Test
+    void testHitchHiker() {
+    }
+}
