aeadafbd7dfbd8c04ef8266197eb53782cbb1891
Jinger <1399756302@qq.com>
2024-05-15 10:46:13 +0800

    doc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"javadocTest"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"VernacularJazz"},{"l":"所有类和接口","u":"allclasses-index.html"}];updateSearchResults();
