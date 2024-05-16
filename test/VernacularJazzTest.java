5118f010c65045c498ab91a5ae9198428bffd35a
Jinger <1399756302@qq.com>
2024-05-16 17:24:48 +0800

    test 3rd change

--- test/VernacularJazzTest.java
+++ test/VernacularJazzTest.java
@@ -6,9 +6,16 @@ class VernacularJazzTest {
 
     @Test
     void testAppleJack() {
+        VernacularJazz jazz = new VernacularJazz();
+        String[] result = jazz.appleJack();
+        assertNotNull(result);
+        assertEquals(64, result.length);
     }
 
     @Test
     void testCakeWalk() {
+        VernacularJazz jazz = new VernacularJazz();
+        int[] array = jazz.blackBottom();
+        jazz.cakeWalk(array);
     }
 }
